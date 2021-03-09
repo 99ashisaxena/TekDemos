91e6c7c19d9f1e7c0afbcfce92f40a6d46496008
Saxena <assaxena@teksystems.com>
2021-03-09 14:28:50 +0530

    Changes done

---
+++ jsdemo.js
@@ -0,0 +1,14 @@
+num1=prompt("Enter First Number");
+num2=prompt("Enter Second Number");
+console.log("Addition " + (+num1 + +num2));
+document.write("Multiplication " + (+num1 * +num2) );
+document.write("\n\n");
+num1++;
+console.log("Unary operator " + num1);
+num2-=4;
+console.log(num2);
+console.log("which is greater"+num1+"or" + num2 +(num1>num2));
+
+num1=9;
+num2=9
+console.log(num1==num2);
